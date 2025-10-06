31d5a8cc0d0774d7ce12de8e407167ed0f3a02db
isacamargo09 <isabelly.camargo@estudante.ifms.edu.br>
2025-10-06 11:09:43 -0300

    carrinho

--- src/rotas.ts
+++ src/rotas.ts
@@ -1,5 +1,6 @@
 import produtoController from "./produtos/produto.controller.js";
 import usuarioController from "./usuarios/usuario.controller.js";
+import carrinhoController from "./carrinhos/carrinho.controller.js";
 
 import { Router } from "express";
 
@@ -14,6 +15,12 @@ rotas.get("/usuarios", usuarioController.listar);
 rotas.post("/produtos",produtoController.adicionar);
 rotas.get("/produtos",produtoController.listar);
 
+//rotas para carrinho
+rotas.post("/carrinho"), produtoController.adicionar;
+rotas.get("/carrinho"), produtoController.listar;
+
+//Adicioonar item
+rotas.post("/adicionar-item", carrinhoController.adicionarItem);
 //Ainda vamos ter que criar as rotas para carrinho e produtos
 //Tarefa para casa :)
 
